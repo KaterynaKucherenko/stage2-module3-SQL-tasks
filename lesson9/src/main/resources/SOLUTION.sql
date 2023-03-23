dd0e54b74f57bd2dec58713d259c27fa03e0ee35
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-24 00:22:59 +0300

    comm6

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SELECT * FROM STUDENT WHERE id=(SELECT student_id FROM mark GROUP BY student_id HAVING avg(mark)>8);
+SELECT * FROM STUDENT WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING avg(mark)>8);
 SELECT s.id, s.name FROM student AS s JOIN mark AS m ON s.id=m.student_id GROUP BY (s.id, s.name) HAVING min(m.mark)>7;
-SELECT id, name FROM student JOIN payment ON student.id=payment.student_id GROUP BY (student.id, student.name) WHERE payment.payment_date BEETWEEN '2019-01-01' AND '2019-12-31' GROUP BY student.id HAVING COUNT(payment.amount) >=2;
+SELECT s.id, s.name FROM student AS s JOIN payment AS p ON s.id=p.student_id GROUP BY (s.id, s.name) WHERE p.payment_date BEETWEEN '2019-01-01' AND '2019-12-31' GROUP BY (student.id, student.name) HAVING COUNT(payment.amount) >=2;
 
