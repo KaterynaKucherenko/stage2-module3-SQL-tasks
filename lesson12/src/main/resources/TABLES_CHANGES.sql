4765838ce28b4a56c5970b83d074dac4e1357b1f
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-23 23:13:22 +0300

    comm5

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -4,8 +4,8 @@ ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) re
 ALTER TABLE mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) references subject(id) ON DELETE CASCADE;
 ALTER TABLE payment DROP CONSTRAINT payment_type_id_fkey;
 ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) references paymenttype(id);
-ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) references student(id);
+ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) references paymenttype(id) ON DELETE CASCADE;
+ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) references student(id) ON DELETE CASCADE;
 
 
 
